e16ce124af79afa9dd746056b113e4285be85c3f
GlitchyBat <GlitchyBat@Tutanota.com>
2016-05-26 13:37:03 -0700

    Boot scene implemented.
    Boot scene just writes text to the screen and serves as start + initial screen.
    ResetListener can be put somewhere in game scene or on player or something, reloads Boot.

---
+++ Miranda/Assets/Boot/BootSequence.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BootSequence : MonoBehaviour {
+	[SerializeField]
+	QuotePayload dialogue = QuotePayload.Uninitialized;
+
+	[SerializeField]
+	KeyCode bootKey = KeyCode.Space;
+
+	[SerializeField]
+	string targetScene = string.Empty;
+
+	[SerializeField]
+	float textResetDelay = 15f;
+
+	void Start() {
+		QuoteBoxManager.Instance.DefaultQuoteBox.DefaultTextSpeed = QuoteBoxSpeed.FAST;
+		StartCoroutine(DelayTextReset(textResetDelay));
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(bootKey)) {
+			SceneManager.LoadScene(targetScene);
+		}
+	}
+
+	IEnumerator DelayTextReset(float mSeconds) {
+		while(isActiveAndEnabled) {
+			QuoteBoxManager.Instance.ShowText(dialogue);
+			yield return new WaitForSeconds(mSeconds);
+			SceneManager.LoadScene("Boot");
+		}
+	}
+}
